cc0b29f1128155092a3456d553ca8332ca71d38b
Khaja Mohammed <kmoha023@fiu.edu>
2018-08-11 22:02:02 -0400

    Formatted using functions

--- main.go
+++ main.go
@@ -61,7 +61,7 @@ func IOpackage() {
 func FilesAndFolder() {
 	// Files and Folders
 	//-------Buffer struct of bytes to read or write
-	fmt.Println("\n Method I ----- Files and Folders")
+	fmt.Println("\nFiles and Folders")
 	//To Open a file use Open function from os package
 	file, err := os.Open("text.txt")
 	if err != nil {
@@ -71,29 +71,97 @@ func FilesAndFolder() {
 	defer file.Close()
 
 	//get the file size
+	stat, error1 := getFileInfo(file)
+	if error1 != nil {
+		errorHandler(error1)
+	} else {
+		fmt.Printf("File %s size is %d ", stat.Name(), stat.Size())
+	}
+
+	//read file method 1
+	msg1, err1 := ReadFromFileMethod1(file, stat)
+	if err1 != nil {
+		errorHandler(err1)
+	} else {
+		PrintToConsole(msg1)
+	}
+
+	//read file method 2
+	msg2, err2 := ReadFromFileMethod2("text.txt")
+	if err2 != nil {
+		errorHandler(err2)
+	} else {
+		PrintToConsole(msg2)
+	}
+
+	createdFile, err3 := CreateFile("text2.txt", "Hello world! from created file")
+	if err3 != nil {
+		errorHandler(err3)
+	} else {
+		createdFilestat, error1 := getFileInfo(createdFile)
+		if error1 != nil {
+			errorHandler(error1)
+		} else {
+			fmt.Printf("Created file %s size is %d ", createdFilestat.Name(), createdFilestat.Size())
+		}
+	}
+	msg4, err4 := ReadFromFileMethod2("text2.txt")
+	if err4 != nil {
+		errorHandler(err4)
+	} else {
+		PrintToConsole(msg4)
+	}
+
+}
+
+func errorHandler(err error) {
+	log.Fatal(err)
+}
+
+//PrintToConsole ...
+func PrintToConsole(msg string) {
+	fmt.Println(msg)
+}
+
+func getFileInfo(file *os.File) (os.FileInfo, error) {
+	fmt.Println("Getting fileInfo....")
 	stat, err := file.Stat()
 	if err != nil {
-		log.Fatal("Error getting stats..", err)
-		return
+		return nil, err
 	}
+	return stat, nil
+}
 
+//ReadFromFileMethod1 ...
+func ReadFromFileMethod1(file *os.File, stat os.FileInfo) (string, error) {
 	//Read from file
+	fmt.Println("\nReading Method I....")
 	bs := make([]byte, stat.Size())
-	_, err = file.Read(bs)
+	_, err := file.Read(bs)
 	if err != nil {
-		log.Fatal("Error reading file...", err)
-		return
+		return "", err
 	}
+	return string(bs), nil
+}
 
-	str := string(bs)
-	fmt.Println(str)
-	fmt.Println("\n Method II ----- Files and Folders")
-
-	bs2, err := ioutil.ReadFile("text.txt")
+//ReadFromFileMethod2 ...
+func ReadFromFileMethod2(file string) (string, error) {
+	fmt.Println("Reading Method II....")
+	bs2, err := ioutil.ReadFile(file)
 	if err != nil {
-		log.Fatal("Error in reading in method 2", err)
+		return "", err
 	}
-	str2 := string(bs2)
-	fmt.Println(str2)
+	return string(bs2), nil
+}
 
+//CreateFile ...
+func CreateFile(name string, text string) (*os.File, error) {
+	//createFile
+	fmt.Println("Creating File... ", name)
+	file, err := os.Create(name)
+	if err != nil {
+		return file, err
+	}
+	file.WriteString(text)
+	return file, nil
 }
